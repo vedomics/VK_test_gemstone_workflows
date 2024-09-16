6f90bb9fdaaab4787d4925917ab43c552974e6f9
Veda Khadka <vkhadka@wm448-fd5.broadinstitute.org>
2024-09-16 10:13:30 -0400

    ironing out bugs

--- workflows/PhyloSKA_Test/Docker/reader_test/read_tsv.py
+++ workflows/PhyloSKA_Test/Docker/reader_test/read_tsv.py
@@ -6,19 +6,20 @@ strains_to_cov = {}
 		
 #report_file = "~{straingst_report}"
 report_file = sys.argv[1]
-# outfile = "STRAIN_REF"
-outfile = sys.argv[2]
+outfile = "STRAIN_REF"
+# outfile = sys.argv[2]
 
-with open(report_file) as infile:
+with open(report_file, 'r') as infile:
 	next(infile) #skip headers 
 	for line in infile:
 		l=line.split('\t')
 		strains_to_cov[l[1]] = l[5]
 
-output_strain = max(strains_to_cov, key = strains_to_cov.get)[0] #takes the first value in case of a tie
+
+output_strain = max(strains_to_cov, key = strains_to_cov.get)
 out_cov = strains_to_cov[max(strains_to_cov, key = strains_to_cov.get)]
 
-if out_cov > 0.8:
+if float(out_cov) > 0.8:
 	print(output_strain)
 else:
 	print("Insufficient_COV")
